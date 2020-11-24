8488606a11c5843f78abb407cf945c9ffdd1c10f
Fan Hongtao <fanhongtao@gmail.com>
2020-11-24 12:21:26 +0800

    Add download script for extra files. (They are too large to put into repo.)

---
+++ smfplayer.js/download_extra.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+wget 'https://logue.dev/smfplayer.js/Yamaha XG Sound Set Ver.2.0.sf2'
+
+mkdir midifiles
+cd midifiles
+
+wget 'https://logue.dev/smfplayer.js/midifiles/YAMAHA mididata library.zip'
+wget https://logue.dev/smfplayer.js/midifiles/適当詰め合わせ.zip
+wget https://logue.dev/smfplayer.js/midifiles/mp2mml_sample.zip
+wget https://logue.dev/smfplayer.js/midifiles/3MLE_Sample.zip
+wget https://logue.dev/smfplayer.js/midifiles/MakimabiSequence_Sample.zip
+wget https://logue.dev/smfplayer.js/midifiles/MabiIcco_Sample.zip
+wget https://logue.dev/smfplayer.js/midifiles/Midi01.zip
+wget https://logue.dev/smfplayer.js/midifiles/Midi03.zip
+wget https://logue.dev/smfplayer.js/midifiles/Midi04.zip
+wget https://logue.dev/smfplayer.js/midifiles/Chrono_Trigger.zip
+wget https://logue.dev/smfplayer.js/midifiles/Dragon_Warrior_2.zip
+wget https://logue.dev/smfplayer.js/midifiles/Dragon_Warrior_3.zip
+wget https://logue.dev/smfplayer.js/midifiles/Dragon_Warrior_4.zip
+wget https://logue.dev/smfplayer.js/midifiles/Final_Fantasy_4.zip
+wget https://logue.dev/smfplayer.js/midifiles/Final_Fantasy_5.zip
+wget https://logue.dev/smfplayer.js/midifiles/Final_Fantasy_6.zip
+wget https://logue.dev/smfplayer.js/midifiles/Secret_of_Mana.zip
+wget https://logue.dev/smfplayer.js/midifiles/Super_Donkey_Kong.zip
+wget https://logue.dev/smfplayer.js/midifiles/Zelda_3_-_A_Link_to_the_Past.zip
+wget https://logue.dev/smfplayer.js/midifiles/A-Train_Global.zip
+wget https://logue.dev/smfplayer.js/midifiles/Alundra.zip
+wget https://logue.dev/smfplayer.js/midifiles/Breath_of_Fire_4.zip
+wget https://logue.dev/smfplayer.js/midifiles/Final_Fantasy_10.zip
+wget https://logue.dev/smfplayer.js/midifiles/Final_Fantasy_11.zip
+wget https://logue.dev/smfplayer.js/midifiles/Final_Fantasy_7.zip
+wget https://logue.dev/smfplayer.js/midifiles/Final_Fantasy_8.zip
+wget https://logue.dev/smfplayer.js/midifiles/Final_Fantasy_9.zip
+wget https://logue.dev/smfplayer.js/midifiles/Legend_of_Mana.zip
+wget https://logue.dev/smfplayer.js/midifiles/Silent_Hill.zip
+wget https://logue.dev/smfplayer.js/midifiles/Star_Ocean_the_second_story.zip
+wget https://logue.dev/smfplayer.js/midifiles/Valkyrie_Profile.zip
+wget https://logue.dev/smfplayer.js/midifiles/A-Train.zip
+wget https://logue.dev/smfplayer.js/midifiles/Lemmings.zip
+wget https://logue.dev/smfplayer.js/midifiles/Marathon1.zip
+wget https://logue.dev/smfplayer.js/midifiles/SimCity2000.zip
+wget https://logue.dev/smfplayer.js/midifiles/UO_MIDI_Music.zip
+wget https://logue.dev/smfplayer.js/midifiles/sor.zip
+wget https://logue.dev/smfplayer.js/midifiles/Outrun.zip
+wget https://logue.dev/smfplayer.js/midifiles/PSO.zip
+wget https://logue.dev/smfplayer.js/midifiles/Sonic_the_Hedgehog.zip
